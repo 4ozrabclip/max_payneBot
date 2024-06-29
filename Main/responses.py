580e1805d1d81d32e0e047139c9661b17d421684
4ozde <csi78@uclive.ac.nz>
2024-06-29 23:37:26 +1200

    hey responses

--- Main/responses.py
+++ Main/responses.py
@@ -1,20 +1,20 @@
 from random import choice
 import re
 
-rand_responses = ['saying goodbye is painful', 'mirrors are more fun than television', 
-                  'your past has a way of speaking up on you.  youll hear broken echoes of it everywhere, like a bad replay'
-                  'legitimate?  youre stealing peoples organs.', '*takes pills* they had their body armor, i had mine', 
-                  '*takes pills* breakfast of champions >;)', 'your past could destroy you, drive you mad.  it could set you free',
-                  'after y2k, the end of the world had become a cliche.  everything was subjective, there were only personal apocalypses']
-
-filth = "username here"
-
 def get_response(user_input: str, username: str) -> str:
-    usrmsg: str = user_input.lower()
+    rand_responses = ['saying goodbye is painful', 'mirrors are more fun than television', 
+                    'your past has a way of speaking up on you.  youll hear broken echoes of it everywhere, like a bad replay'
+                    'legitimate?  youre stealing peoples organs.', '*takes pills* they had their body armor, i had mine', 
+                    '*takes pills* breakfast of champions >;)', 'your past could destroy you, drive you mad.  it could set you free',
+                    'after y2k, the end of the world had become a cliche.  everything was subjective, there were only personal apocalypses']
 
+    hey_responses = [f'hey {username}', 'hi', 'sup', 'good day?']
+
+
+    usrmsg: str = user_input.lower()
     if usrmsg == '':
         return "cool"
     elif re.search(r'\b(hello+|hi+|hey+)\b', usrmsg):
-        return f'hey @{username}'
+        return choice(hey_responses)
     else:
         return choice(rand_responses)
